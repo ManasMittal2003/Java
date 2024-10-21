a1169c267dfe6ed5da124f5278c2f80092fe172b
ManasMittal2003 <manasmittal.cllg@gmail.com>
2024-10-21 23:35:06 +0530

    Update

---
+++ array_of_objects.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class stu {
+    String name;
+    int age;
+    public void setter(String n, int a){
+        name = n;
+        age = a;
+    }
+    public void getter(){
+        System.out.print("Name: "+name+" ");
+        System.out.println("Age: "+age);
+    }
+}
+
+public class array_of_objects {
+    public static void main(String args[]){
+        stu students[] = new stu[2];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<students.length;i++){
+            students[i] = new stu();
+            System.out.println("Enter details of " + (i+1) + " student: ");
+            String name = sc.nextLine();
+            int age = sc.nextInt();
+            sc.nextLine();
+            students[i].setter(name,age);
+        }
+        for(int i=0;i<students.length;i++){
+            students[i].getter();
+        }
+    }
+}
